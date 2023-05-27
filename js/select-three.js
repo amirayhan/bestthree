0d58197646099e407dfc7e1b06e4a4f6bb241c58
rayhan <jaber.ac1983@gmail.com>
2023-05-27 14:42:53 +0600

    functionally compeleted with js

--- js/select-three.js
+++ js/select-three.js
@@ -36,15 +36,77 @@ function handleItemButtonClick(event) {
         // Append the list item to the cart list
         playerList.appendChild(listItem);
     }
+
+    // player btn active
     const clickedButton = event.target;
+
+    // Get active classes btn
     const activeButtons = document.querySelectorAll(".active");
 
+    // Not to add more than 3 active classes
     if (activeButtons.length < 3) {
         clickedButton.classList.add("active");
     }
 }
-
 // Add click event listeners to the item buttons
 itemButtons.forEach((button) => {
     button.addEventListener("click", handleItemButtonClick);
+});
+
+// To Get Input Ammount with Intiger Type
+function inputAmount(inputId) {
+    const inputAmmountField = document.getElementById(inputId);
+    const ammountfieldString = inputAmmountField.value;
+    const ammount = parseInt(ammountfieldString);
+
+    return ammount;
+}
+
+// To Get Element Ammount with Intiger Type
+function elementAmmount(elementId) {
+    const elementAmmountField = document.getElementById(elementId);
+    const elementAmmountFieldString = elementAmmountField.innerText;
+    const ammountOfElement = parseInt(elementAmmountFieldString);
+
+    return ammountOfElement;
+}
+
+// Get total ammount of three palyers expcences
+document.getElementById("budget-calculate").addEventListener("click", function () {
+    // Get Per Player Ammount
+    const perPlayerAmmount = inputAmount("per_player");
+
+    // Get Player List
+    const playerList = document.getElementById("selected_palyer");
+
+    // Get Player List Length
+    const liLength = playerList.getElementsByTagName("li").length;
+
+    // Total Player Expences
+    const playerExpences = perPlayerAmmount * liLength;
+
+    // Get Player Expences Display
+    const playerExpencesDisplay = document.getElementById("palyer_expences_display");
+
+    // Add total player expences in player Expence Display
+    playerExpencesDisplay.innerText = playerExpences;
+});
+
+// Get total expences
+document.getElementById("total-calculate").addEventListener("click", function () {
+    // total player expences
+    const totalPlayerExpences = elementAmmount("palyer_expences_display");
+
+    // Get Manager Ammount
+    const managerAmmount = inputAmount("manager");
+
+    // Get Coach Ammount
+    const coachAmmount = inputAmount("coach");
+
+    // Get total expences ammount
+    const totalExpencesAmmount = totalPlayerExpences + managerAmmount + coachAmmount;
+
+    const totalExpencesDisplay = document.getElementById("total_expences_display");
+
+    totalExpencesDisplay.innerText = totalExpencesAmmount;
 });
